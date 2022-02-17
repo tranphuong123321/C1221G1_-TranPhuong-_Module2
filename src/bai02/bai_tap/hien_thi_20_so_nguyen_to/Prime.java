f995258d1ed88813a6967dd47c38afaace8e02fd
tranphuong123321 <phuong23628@gmail.com>
2022-02-17 17:01:52 +0700

    push

--- src/bai02/bai_tap/hien_thi_20_so_nguyen_to/Prime.java
+++ src/bai02/bai_tap/hien_thi_20_so_nguyen_to/Prime.java
@@ -6,10 +6,10 @@ public class Prime {
         int num = 3;
         System.out.println("Nhập vào n số nguyên tố đầu tiên:");
         int n = new Scanner(System.in).nextInt();
-        if (n == 1) {
+
             System.out.println(n+" Số nguyên tố đầu tiên là:");
-            System.out.println(2);
-        }
+            System.out.print(2+"\t");
+
         for ( int i = 2 ; i <=n ;  ) {
             for ( int j = 2 ; j <= Math.sqrt(num) ; j++ ) {
                 if ( num%j == 0 ){
@@ -18,7 +18,7 @@ public class Prime {
                 }
             }
             if ( status != 0 ) {
-                System.out.println(num);
+                System.out.print(num+"\t");
                 i++;
             }
             status = 1;
